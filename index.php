339d633232bf8776e6492c71026ee65f832faca3
Matt Rayner <matt@mattrayner.co.uk>
2013-11-02 12:35:22 +0000

    Updated initial home page - fix for branch merge

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ $language = 'en_GB';
 
 if(isset($_GET['locale']) && $_GET['locale'] == "ca") $language = "ca_ES";
 
+//Check to see if we have specified a desktop site (mobile override)
+if(!isset($_GET['target']) || $_GET['target'] != "desktop"){
+	require("lib/mobile.php");
+	
+	$param = "";
+	
+	if(isset($_GET['locale']) && $_GET['locale'] == "ca")
+		$param = "?locale=".$_GET['locale'];
+	
+	reditectToMobile($param);	
+}
+
 $newLocale=setlocale (LC_ALL, $language);
 
 // Set the text domain as 'messages'
@@ -15,116 +27,18 @@ textdomain($domain);
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title><?php echo gettext("Give Cats! Make your internet warm and fuzzy!"); ?></title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
-
-    <!-- Custom styles for this template -->
-    <link href="assets/css/givecat.css" rel="stylesheet">
-
-	<!-- Facebook Image -->
-	<meta property="og:image" content="http://give.cat/assets/img/CatBackground.jpg"/>
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li><a href="../navbar-static-top/">Static top</a></li>
-            <li class="active"><a href="./">Fixed top</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
-    <div class="container">
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-      </div>
-
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
-    <script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	
-	  ga('create', 'UA-44617939-1', 'give.cat');
-	  ga('send', 'pageview');
-	</script>
-  </body>
-</html>
-
-<!DOCTYPE html>
 <html>
   <head>
     <title><?php echo gettext("Give Cats! Make your internet warm and fuzzy!"); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+	<link href="assets/css/home.css" rel="stylesheet" media="screen">
 	
 	<meta property="og:image" content="http://give.cat/assets/img/CatBackground.jpg"/>
   </head>
   <body class="no-background">
-  	<div class="alert alert-danger">We're making some changes! Bare with us as we make Give Cat bigger and better!</div>
   	<div id="wrap" class="container-fluid">
     	<div class="row-fluid">
     		<div class="span6 text-center vertical-center hidden-phone">
@@ -133,17 +47,25 @@ textdomain($domain);
 				<p><?php echo gettext("Drag this button on to your bookmark bar and click it for insta-cute on any website."); ?></p>
 				<div class="row-fluid">
 					<a class="btn btn-large btn-primary" href='<?php
-						$file = file_get_contents('GiveCatJS/givecat.database.min.js', FILE_USE_INCLUDE_PATH); 
-						echo "javascript: ".$file;
+						$file = file_get_contents('GiveCatJS/givecat.loader.min.js', FILE_USE_INCLUDE_PATH); 
+						echo $file;
 						?>'><?php echo gettext("Give Cats!"); ?></a>
 				</div>
+    		</div>
+    		
+    		<div class="span6 hidden-phone"></div>
+    		
+    		<div class="span12 visible-phone semi-transparent">
+	    		<h1><?php echo gettext("Meow!"); ?></h1>
+	    		<p class="lead"><?php echo gettext("Thanks for coming! At the moment you need to use a computer or a tablet to use us (that's changing)."); ?></p>
+	    		<p><?php echo gettext("Soon there'll be an iPhone app with all sorts of squishy goodies but until then, come back on something bigger :)"); ?></p>
     		</div>
     	</div>
     	<div id="push"></div>
   	</div>
   	<footer id="footer">Copyright &copy;2013 <a href="http://give.cat/">Give.Cat</a>. Created by <a href="http://www.twitter.com/mattrayner" target="_blank>">Matt Rayner</a>, inspired by the genius of <a href="http://heygirl.io/" target="_blank">Hey Girl</a>. <a href="http://give.cat/<?php if(!isset($_GET['locale'])){echo "?locale=ca";} ?>"><?php echo gettext("Parla catal&#133;?"); ?></a> - Fork me on <a href="https://github.com/mattrayner/Give.Cat" target="_blank">GitHub</a>.</footer>
     <script src="http://code.jquery.com/jquery.js"></script>
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
     <script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
